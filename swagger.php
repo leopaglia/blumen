343d63a1a5c6ff3f0eabb6a6e7e5da122b28e2a1
leonardo <leonardo.paglialunga@quadminds.com>
2017-01-31 15:49:31 -0300

    base swagger file

---
+++ swagger.php
@@ -0,0 +1,11 @@
+<?php
+
+// see: https://github.com/zircote/swagger-php/blob/master/Examples/petstore.swagger.io/swagger-v2.php
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="localhost",
+ *     basePath="/"
+ * )
+ */
